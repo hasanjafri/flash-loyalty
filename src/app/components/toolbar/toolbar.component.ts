dc7a72649cee5c3c07a5a8ec8fae3a9984ec9c40
Hasan Jafri <hasan.jafri7@gmail.com>
2019-11-11 15:45:41 -0500

    WIP: Change UI theme as role changes

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,15 +1,19 @@
-import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChildren, OnInit, OnDestroy } from '@angular/core';
 import { OverlayService } from 'src/app/services/overlay.service';
 import { SearchBarComponent } from '../search-bar/search-bar.component';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements OnInit, OnDestroy {
   @ViewChildren('toolbarIcons') toolbarIcons: QueryList<ElementRef>;
 
+  currentRole: string;
+  currentRoleSub: Subscription;
   toolbarLinks = [
     {
       icon: 'search',
@@ -23,9 +27,21 @@ export class ToolbarComponent {
     }
   ];
 
-  constructor(private overlayService: OverlayService) {}
+  constructor(private overlayService: OverlayService, private authService: AuthService) {}
 
   openSearch() {
     this.overlayService.open(SearchBarComponent, this.toolbarIcons.first);
+  }
+
+  async ngOnInit() {
+    this.currentRoleSub = await this.authService.currentRoleSub.subscribe(
+      (currentRole) => (this.currentRole = currentRole)
+    );
+  }
+
+  ngOnDestroy() {
+    if (this.currentRoleSub) {
+      this.currentRoleSub.unsubscribe();
+    }
   }
 }
